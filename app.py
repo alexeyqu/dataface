4aca4b2b603c188a1ec9be16682f990860e280c6
Alexey Kulikov <alexeyqu@gmail.com>
2019-11-17 05:34:31 +0300

    fixes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import datetime
 import imutils
 from imutils.video import VideoStream
+from PIL import Image
 import cv2
 import os
 
@@ -10,12 +11,37 @@ from cv.recognition import FacialRecognizer
 
 import sys
 import time
+import numpy as np
 
 import cv2
+import tkinter
+from tkinter import simpledialog
 
 
-video_stream_path = 'http://192.168.1.109:4747/video'
+video_stream_path = 0#'http://192.168.1.109:4747/video'
 recognizer = FacialRecognizer('db.sqlite')
+clicked = False
+mouseX, mouseY = None, None
+
+root = tkinter.Tk()
+root.withdraw()
+
+
+def add_new_person(img):
+    name = simpledialog.askstring('Who is this?', 'Enter the name')
+    recognizer.assign_name_to_image(np.asarray(img), name)
+    
+
+
+def clck_handler(event, x, y, flags, param):
+    global mouseX, mouseY, clicked
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        mouseX, mouseY, clicked = x, y, True
+        print(mouseX, mouseY, clicked)
+
+
+cv2.namedWindow('video')
+cv2.setMouseCallback('video', clck_handler)
 
 
 class FaceApp:
@@ -23,11 +49,16 @@ class FaceApp:
         self.vs = vs
 
     def run(self):
-        process = True
+        global clicked, mouseX, mouseY
+        process_ratio = 5
+        process = 0
+        faces = []
+        frame = None
         try:
             while True:
                 frame = self.vs.read()
-                if process:
+                if process % process_ratio == 0:
+                    process = 0
                     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
                     imagearr = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
 
@@ -36,10 +67,16 @@ class FaceApp:
                     for face in faces:
                         cv2.rectangle(frame, (face.left * 4, face.top * 4), (face.right * 4, face.bottom * 4), (0, 255, 0), 2)
                         font = cv2.FONT_HERSHEY_DUPLEX
-                        cv2.putText(frame, face.name, (face.left + 6, face.bottom - 6), font, 1.0, (255, 255, 255), 1)
+                        cv2.putText(frame, face.name or 'unknown', (face.left * 4 + 6, face.bottom * 4 - 6), font, 1.0, (255, 255, 255), 1)
+                    cv2.imshow('video', frame)
 
-                process = not process
-                cv2.imshow('Video', frame)
+                process += 1
+
+                if clicked:
+                    clicked = False
+                    for face in faces:
+                        if face.top < mouseY * 0.25 < face.bottom and face.left < mouseX * 0.25 < face.right and face.name is None:
+                            add_new_person(Image.fromarray(imagearr).crop((face.left * 4, face.top * 4, face.right * 4, face.bottom * 4)))
 
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
